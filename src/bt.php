6e494f84cb11b99ac2006e42be986a5557ca7865
Jacco-V <jacco.vinke@netsilik.nl>
2017-09-27 22:48:53 +0200

    Add function body

--- src/bt.php
+++ src/bt.php
@@ -2,16 +2,66 @@
 
 if (!function_exists('bt')) {
 	/**
-	 * Debug funcion for dumping contents and type of a variable
-	 * @param mixed $var The variable to output for debug information
+	 * Debug funcion for dumping call backtrace
+	 * @param int $limit The maximum number of backtrace steps to display
 	 * @param bool $return Optional flag to have the result returned (without any escaping) instead of echoed
-	 * @param depth The indentation level, used for recursive calls
 	 */
-	function bt($var, $return = false)
+	function bt($limit, $return = false)
 	{
-	
-		echo '<pre style="margin: 0;"><span style="color:#000038; font-size: 13px; line-height: 15px; background-color:#F4F4F4;">';
-		debug_print_backtrace(2, 8);
-		echo '</span></pre>';
+		$bt = debug_backtrace();
+		
+		$lines = [];
+		for ($i = 0; $i < min($limit, count($bt)); $i++) {
+			$step = $bt[$i];
+			
+			$args = [];
+			foreach ($step['args'] as $arg) {
+				switch (gettype($arg)) {
+					case 'object':
+						$args[] = get_class($arg);
+						break;
+					case 'array':
+						$args[] = '['.(count($arg)?count($arg).' elements':'').']';
+						break;
+					case 'resource':
+						$args[] = get_resource_type($var);
+						break;
+					case 'boolean':
+						$args[] = $arg ? 'true' : 'false';
+						break;
+					case 'string':
+						$args[] = "'".addcslashes($arg, "'\\")."'";
+						break;
+					default: // double, integer
+						$args[] = (null === $arg) ? 'NULL' : $arg;
+				}
+			}
+			
+			
+			$line = '#'.str_pad($i, 2).' ';
+			if (isset($step['class'])) {
+				$line .= $step['class'].$step['type'];
+			}
+			$line .= $step['function'].'('.implode(', ', $args).') called at ';
+			
+			if (isset($step['file'])) {
+				$line .= '['.$step['file'].':'.$step['line'].']';
+			} else {
+				$line .= '[undefined]';
+			}
+			
+			$lines[] = $line;
+		}
+		$out = implode("\n", $lines);
+		
+		if ($return) {
+			return $out;
+		}
+		
+		if (PHP_SAPI === 'cli') { // output as text
+			echo "\n".$out;
+		} else { // output as html
+			echo '<pre style="margin: 0;"><span style="color:#000038; font-size: 13px; line-height: 15px; background-color:#F4F4F4;">'.htmlspecialchars($out, ENT_NOQUOTES | ENT_XHTML).'</span></pre>';
+		}
 	}
 }
